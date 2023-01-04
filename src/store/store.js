15af08c21e243ab0f0e734df5c1e9217a5942f09
Kalman Kostenszky <k.kostenszky@gmail.com>
2023-01-04 21:39:24 +0100

    persist state

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import cartSlice from "./cart/cart.slice";
 import categoriesSlice from "./categories/categories.slice";
 import userSlice from "./user/user.slice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 
+
+const combinedReducers = combineReducers({
+    user: userSlice,
+    categories: categoriesSlice,
+    cart: cartSlice,
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 0,
+    blacklist: ['user']
+}
 
 export const store = configureStore({
-    reducer: {
-        user: userSlice,
-        categories: categoriesSlice,
-        cart: cartSlice
-    },
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger), 
+    reducer: persistReducer(persistConfig, combinedReducers),
+    middleware: getDefaultMiddleware => {
+        if (process.env.NODE_ENV !== 'production') {
+            return getDefaultMiddleware().concat(logger)
+        }
+        return getDefaultMiddleware();
+    }
 });
+
+export const persistor = persistStore(store);
